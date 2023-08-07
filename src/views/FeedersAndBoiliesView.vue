ecd0249ccec0f6941b6a0c3cdd50ff4a367094ec
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-08-07 13:05:21 +0200

    Implementing hero container on spinning subpage + adding background images to assets

--- src/views/FeedersAndBoiliesView.vue
+++ src/views/FeedersAndBoiliesView.vue
@@ -10,7 +10,7 @@ export default {
   data() {
     return {
       feedersAndBoiliesHeroContent: {
-        url: require("@/assets/images/footer_images/footer_background_image.jpeg"),
+        url: require("@/assets/images/subpage_hero-section-images/feeders-and-boilies_hero-image.jpeg"),
         headline: "Feeders and boilies",
         subline:
           "Our beloved products are always available in a wide range on our website.",
--- src/views/SpinningView.vue
+++ src/views/SpinningView.vue
@@ -1,11 +1,24 @@
 <template>
-  <section>
-    <div class="container">
-      <div class="container__flex">
-        <h1>Spinning subpage</h1>
-      </div>
-    </div>
-  </section>
+  <subpage-hero-section v-bind="feedersAndBoiliesHeroContent" />
 </template>
+
+<script>
+import SubpageHeroSection from "@/components/feeders-and-boilies/hero-section/SubpageHeroSection.vue";
+
+export default {
+  components: { SubpageHeroSection },
+  data() {
+    return {
+      feedersAndBoiliesHeroContent: {
+        url: require("@/assets/images/subpage_hero-section-images/spinning_hero-image.jpg"),
+        headline: "Spinning",
+        subline:
+          "It's time to catch some glorious predators!",
+        span: "Buckle up!",
+      },
+    };
+  },
+};
+</script>
 
 <style lang="scss"></style>
