f53e8430db76f551692f30a2ec039bfb2a81a208
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-09-05 14:11:35 +0200

    Adding Fishing Expo component to feeders and boilies subpage

---
+++ src/components/fishing-expo-section/FishingExpoSection.vue
@@ -0,0 +1,127 @@
+<template>
+  <section class="fishing-expo-section">
+    <div class="container fishing-expo-section__container">
+      <div class="fishing-expo-section__inner">
+        <h4>Save up to -75%</h4>
+        <p>
+          Visit our upcoming fishing expo in June and buy at a great price, even
+          up to 75% off! If you bring a friend with you, you'll receive a bonus
+          gift!
+        </p>
+        <white-button
+          :buttonText="textOfExpoButton"
+          isRounded
+          @button-event="jumpToLink"
+        />
+      </div>
+      <div class="fishing-expo-section__inner-image">
+        <img
+          src="@/assets/images/expo-images/fishing_expo_discount.png"
+          alt="Discount of Fishing Expo"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import WhiteButton from "../site-buttons/WhiteButton.vue";
+
+export default {
+  data() {
+    return {
+      textOfExpoButton: "Visit site of expo",
+    };
+  },
+
+  components: {
+    WhiteButton,
+  },
+
+  methods: {
+    jumpToLink() {
+      window.open("https://horgaszshow.hu/hu/");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.fishing-expo-section {
+  padding-block: 5rem;
+  background: url("@/assets/images/expo-images/fishing_expo_2023.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  border-bottom: 15px solid $primary_color_dark_blue;
+
+  &__container {
+    @include flexColumnOnMobile(space-between, center, 1rem, column, 850px);
+  }
+
+  &__inner {
+    @include flexColumn(center, flex-start, 2rem);
+    color: $primary_color_white;
+    background-color: $primary_color_transparent_black_darker;
+    padding: 2rem;
+    border-radius: 10px;
+    width: 50%;
+    margin-right: auto;
+
+    @media screen and (max-width: 850px) {
+      width: 100%;
+    }
+
+    h4 {
+      font-size: 54px;
+    }
+  }
+
+  &__inner-image {
+    @include flexColumn(center, center);
+
+    aspect-ratio: 1/1;
+    width: 25%;
+    height: 25%;
+    border-radius: 50%;
+    background-color: $primary_color_white;
+    animation: slow-levitate 4s ease-in-out infinite;
+
+    @keyframes slow-levitate {
+      0% {
+        transform: translateY(0px);
+      }
+      50% {
+        transform: translateY(-10px);
+      }
+      100% {
+        transform: translateY(0px);
+      }
+    }
+
+    @media screen and (max-width: 850px) {
+      width: 60%;
+      height: 60%;
+    }
+
+    img {
+      display: block;
+      margin: auto;
+      width: 75%;
+      animation: slow-levitate 4s 0.5s ease-in-out infinite;
+
+      @keyframes slow-levitate {
+        0% {
+          transform: translateY(10px);
+        }
+        50% {
+          transform: translateY(-20px);
+        }
+        100% {
+          transform: translateY(10px);
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/FeedersAndBoiliesView.vue
+++ src/views/FeedersAndBoiliesView.vue
@@ -3,6 +3,7 @@
   <two-column-grid-section />
   <top-discount-section />
   <product-row />
+  <fishing-expo-section />
 </template>
 
 <script>
@@ -10,9 +11,10 @@ import SubpageHeroSection from "@/components/feeders-and-boilies/hero-section/Su
 import TwoColumnGridSection from '@/components/feeders-and-boilies/two-columns-grid-section/TwoColumnGridSection.vue';
 import TopDiscountSection from '@/components/feeders-and-boilies/top-discount-section/TopDiscountSection.vue';
 import ProductRow from '@/components/feeders-and-boilies/product-row/ProductRow.vue';
+import FishingExpoSection from '@/components/fishing-expo-section/FishingExpoSection.vue';
 
 export default {
-  components: { SubpageHeroSection, TwoColumnGridSection, TopDiscountSection, ProductRow },
+  components: { SubpageHeroSection, TwoColumnGridSection, TopDiscountSection, ProductRow, FishingExpoSection },
   data() {
     return {
       feedersAndBoiliesHeroContent: {
