e615ab9d77186e8b59939127cdc0fa4adf5f0698
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-09-11 17:09:10 +0200

    Adding Spanish language pack to site

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import store from "./store";
 import { createI18n } from "vue-i18n";
 import en from "./locales/en.json";
 import hu from "./locales/hu.json";
+import es from "./locales/es.json";
 
 const i18n = createI18n({
   locale: "en",
   messages: {
     en,
     hu,
+    es
   },
 });
 
