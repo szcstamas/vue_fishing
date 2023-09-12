83d2c5c9995d5cf33c01082868fdf20d18aef0be
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-09-12 12:51:16 +0200

    Importing i18n to main.js from updated path

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import i18n from "./locales";
+import i18n from "./i18n";
 
 createApp(App).use(store).use(i18n).use(router).mount("#app");
